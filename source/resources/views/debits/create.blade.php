c0561cdacccba00309759a3e925b881c00f9e15a
Lucas Freitas <lucas.rodrigues.developer97@gmail.com>
2024-12-27 11:05:51 -0300

    create form to save new debit

---
+++ source/resources/views/debits/create.blade.php
@@ -0,0 +1,29 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Criar nova conta') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <form action="">
+                        <div>
+                            <x-input-label for="name" :value="__('Nome')" />
+                            <x-text-input id="name" class="block mt-1" type="text" name="name" :value="old('name')" required autofocus />
+                            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                        </div>
+                        <br>
+                        <div>
+                            <x-primary-button>
+                                {{ __('Salvar') }}
+                            </x-primary-button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
